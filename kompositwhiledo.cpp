348563ebcb233b44ddd0a2bc49bffb7b17c21379
Nadikris1 <nadikris2@gmail.com>
2021-04-13 10:42:05 +0700

    Create kompositwhiledo.cpp

---
+++ kompositwhiledo.cpp
@@ -0,0 +1,20 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+	int i;
+	i=1;
+	while(i<=10);
+{
+	if(i==1 ||(!(i==2 || i==3 ||(i%2!=0 && i%3!=0))))
+{
+	cout<<i<<" ";
+}
+	else
+{
+	cout<<" ";
+}
+	i=i+1
+}
+getch();
+}
