d8061827d657dc8e03ffaebd843838c73c9803d4
Nadikris1 <nadikris2@gmail.com>
2021-04-13 10:37:45 +0700

    Create converttime.cpp

---
+++ converttime.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+int jam,menit,j,m,konversi;
+cout<<"Masukan jam: ";
+cin>>j;
+cout<<"masukan menit: ";
+cin>>m;
+jam=j*3600;
+menit=m*60;
+konversi=jam+menit;
+cout<<"hasil konversi: "<<konversi<<"detik ";
+
+getch();
+}
