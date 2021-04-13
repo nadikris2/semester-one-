612473e77d31c540902d7503c687b433491ed124
Nadikris1 <nadikris2@gmail.com>
2021-04-13 10:36:59 +0700

    test

---
+++ testarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream.h>
+#include <stdio.h>
+main()
+{
+mulai:
+clrscr();
+int menu;
+cout<<"---------Pilihan Sortir-----"<<endl;
+cout<<"Lihat Pilihan"<<endl;
+cout<<"1.Nama Siswa"<<endl;
+cout<<"2.ID"<<endl;
+cout<<"3.Nilai"<<endl;
+cout<<"Masukan Pilihan Anda [1 2 3]= ";
+cin>>pilihan;
+
+if(pilihan==1)
+{
+ cout<<"---------------Pilihan 1--------"<<endl; 
+ cout<<"---------------Nama Siswa-------"<<endl;
+ cout<<"1.Biron 100 "<<endl;
+ cout<<"2.Felix 85"<<endl;
+ cout<<"3.Ferdy 90"<<endl;
+ cout<<"4.Henry 75"<<endl;
+ cout<<"5.Susilo 100"<<endl;
+ cout<<"========================================"<<endl;
+}
+else if(pilihan==2
+{
+ cout<<"--------------PILIHAN 2--------"<<endl;
+ cout<<"---------------ID NAME---------"<<endl;
+ cout<<"
