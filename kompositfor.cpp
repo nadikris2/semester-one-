369d320596e07c5a198a8f1aee22ddcda6ac5f0c
Nadikris1 <nadikris2@gmail.com>
2021-04-13 10:39:48 +0700

    test

---
+++ kompositfor.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+    int i;
+
+   for(i=1; i<=10; i++)
+   {
+   	if((i==1 || (!(i==2 || i==3 || (i%2!=0 && i%3!=0)))) && i%2!=0)
+      {
+      	cout<<i<<" ";
+      }
+      else
+      cout<<"";
+   }
+   getch();
